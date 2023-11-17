78f216d1c61a5148be5baa38f33c8c4e05311916
CancerDelPulmon <114493453+CancerDelPulmon@users.noreply.github.com>
2023-11-17 10:37:28 -0500

    Erreur de import

--- client/src/page-index.js
+++ client/src/page-index.js
@@ -4,4 +4,8 @@ window.addEventListener("load", () => {
     document.querySelector("form").onsubmit = function () {
         return signin(this);
     }
+    document.querySelector(".enregistrer").onclick = () => 
+    {
+        window.location.href = "register.html";
+    }
 });
